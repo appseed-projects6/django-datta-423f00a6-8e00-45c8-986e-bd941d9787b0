b4b52beab950d55279e279210f48fba1d872efce
AppSeed Generated Projects <133564452+appseed-projects6@users.noreply.github.com>
2024-05-27 01:08:25 +0300

    initial commit

---
+++ home/models.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    compagnie_id = models.IntegerField(null=True, blank=True)
+    anciennete = models.CharField(max_length=255, null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Logbook(models.Model):
+
+    #__Logbook_FIELDS__
+
+    #__Logbook_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Logbook")
+        verbose_name_plural = _("Logbook")
+
+
+
+#__MODELS__END
